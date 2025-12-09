99492c4cb170c09ff8fbb95fbb63dc5b15b88265
mmed <mila.medic2000@gmail.com>
2025-12-09 12:31:45 +1100

    project initialised

---
+++ backend/src/main/java/org/example/jobsearchengine/JobSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchEngineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/example/jobsearchengine/JobSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
