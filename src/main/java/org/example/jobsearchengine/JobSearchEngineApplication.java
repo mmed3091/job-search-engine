b087c066ffd413b2781565cf9201d7273764f4f5
mmed <mila.medic2000@gmail.com>
2025-12-10 12:58:28 +1100

    erased frontend

--- src/main/java/org/example/jobsearchengine/JobSearchEngineApplication.java
+++ src/main/java/org/example/jobsearchengine/JobSearchEngineApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobSearchEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobSearchEngineApplication.class, args);
+
+        System.out.println("Working");
+        SpringApplication.run(JobSearchEngineApplication.class, args);
 	}
 
 }
